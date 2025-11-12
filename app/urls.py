9a2278c0dcee747e7fb10ef187861dc598b1fab3
nguyenducthang1984 <nguyenducthang1984@vnpt.vn>
2025-11-12 15:19:28 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from . import view_security
 
-app_name = 'password_change'
+app_name = 'app'
 
 urlpatterns = [
     path('change-password/', views.change_password, name='change_password'),
-     path('', views.login_view, name='login'),
+    path('', view_security.login_view, name='login'),
+    path('auth/redirect/<str:token>/', view_security.redirect_intermediate_view, name='redirect_intermediate_view'),
+
 ]
